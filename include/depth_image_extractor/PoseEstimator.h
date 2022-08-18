c5abad68264ce36c6a55aae76e6755b18fa67aa2
AntoineRichard <antoine.richard@gatech.edu>
2022-08-18 12:52:47 +0200

    fixed some bugs. Improved the detection

--- include/depth_image_extractor/PoseEstimator.h
+++ include/depth_image_extractor/PoseEstimator.h
@@ -2,8 +2,11 @@
 #define PoseEstimator_H
 
 #include <vector>
+#include <execution>
 #include <opencv2/opencv.hpp>
 #include <depth_image_extractor/utils.h>
+
+
 
 class PoseEstimator {
   private:
@@ -14,13 +17,24 @@ class PoseEstimator {
     int image_height_;
     int image_width_;
 
-    std::vector<float> P_;
+    float fx_;
+    float fy_;
+    float fx_inv_;
+    float fy_inv_;
+    float cx_;
+    float cy_;
+    std::vector<float> K_;
 
   public:
     PoseEstimator();
     PoseEstimator(float, float, float, float, int, int);
     std::vector<float> extractDistanceFromDepth(const cv::Mat&, const std::vector<std::vector<BoundingBox>>&);
     std::vector<std::vector<float>> estimatePosition(const std::vector<float>& , const std::vector<std::vector<BoundingBox>>&);
+    void deprojectPixel2PointBrownConrady(const float&, const std::vector<float>&, std::vector<float>&);
+    void deprojectPixel2PointPinHole(const float&, const std::vector<float>&, std::vector<float>& );
+    void distancePixel2PointBrownConrady(const float&, const std::vector<float>&, std::vector<float>&);
+    void distancePixel2PointPinHole(const float&, const std::vector<float>&, std::vector<float>& );
+    void updateCameraParameters(float, float, float, float, std::vector<double>);
 };
 
 #endif
--- src/PoseEstimator.cpp
+++ src/PoseEstimator.cpp
@@ -1,4 +1,5 @@
 #include <depth_image_extractor/PoseEstimator.h>
+#include <ros/ros.h>
 
 PoseEstimator::PoseEstimator() {
 }
@@ -11,11 +12,96 @@ PoseEstimator::PoseEstimator(float rejection_threshold, float keep_threshold, fl
   image_height_ = image_height;
   image_width_ = image_width;
 
-  P_.resize(3,0.0);
+  K_.resize(5,0.0);
+  fx_ = 607.7302246;
+  cx_ = 327.8651123;
+  fy_ = 606.1353759;
+  cy_ = 246.6830596;
+}
+
+void PoseEstimator::updateCameraParameters(float cx, float cy, float fx, float fy, std::vector<double> K) {
+  cx_ = cx;
+  cy_ = cy;
+  fx_ = fx;
+  fy_ = fy;
+  fx_inv_ = 1/fx_;
+  fy_inv_ = 1/fy_;
+}
+
+void PoseEstimator::deprojectPixel2PointBrownConrady(const float& depth, const std::vector<float>& pixel, std::vector<float>& point){
+  float x = (pixel[0] - cx_) / fx_;
+  float y = (pixel[1] - cy_) / fy_;
+
+  float xo = x;
+  float yo = y;
+  // Brown-Conrady model used by the realsense.
+  for (int i = 0; i < 10; i++) {
+    float r2 = x * x + y * y;
+    float icdist = (float)1 / (float)(1 + ((K_[4] * r2 + K_[1]) * r2 + K_[0]) * r2);
+    float delta_x = 2 * K_[2] * x * y + K_[3] * (r2 + 2 * x * x);
+    float delta_y = 2 * K_[3] * x * y + K_[2] * (r2 + 2 * y * y);
+    x = (xo - delta_x) * icdist;
+    y = (yo - delta_y) * icdist;
+  }
+  point[0] = depth * x;
+  point[1] = depth * y;
+  point[2] = depth;
+}
+
+void PoseEstimator::deprojectPixel2PointPinHole(const float& depth, const std::vector<float>& pixel, std::vector<float>& point){
+  float x = (pixel[0] - cx_) / fx_;
+  float y = (pixel[1] - cy_) / fy_;
+  point[0] = depth * x;
+  point[1] = depth * y;
+  point[2] = depth;
+}
+
+void PoseEstimator::distancePixel2PointPinHole(const float& depth, const std::vector<float>& pixel, std::vector<float>& point){
+  float x = (pixel[0] - cx_) / fx_;
+  float y = (pixel[1] - cy_) / fy_;
+  float z = depth / sqrt(x*x + y*y + 1);
+  point[0] = z * x;
+  point[1] = z * y;
+  point[2] = z;
+}
+
+void PoseEstimator::distancePixel2PointBrownConrady(const float& depth, const std::vector<float>& pixel, std::vector<float>& point){
+  float x = (pixel[0] - cx_) / fx_;
+  float y = (pixel[1] - cy_) / fy_;
+
+  float xo = x;
+  float yo = y;
+  // Brown-Conrady model used by the realsense.
+  for (int i = 0; i < 10; i++) {
+    float r2 = x * x + y * y;
+    float icdist = (float)1 / (float)(1 + ((K_[4] * r2 + K_[1]) * r2 + K_[0]) * r2);
+    float delta_x = 2 * K_[2] * x * y + K_[3] * (r2 + 2 * x * x);
+    float delta_y = 2 * K_[3] * x * y + K_[2] * (r2 + 2 * y * y);
+    x = (xo - delta_x) * icdist;
+    y = (yo - delta_y) * icdist;
+  }
+  float z = depth / sqrt(x*x + y*y + 1);
+  point[0] = z * x;
+  point[1] = z * y;
+  point[2] = z;
 }
 
 std::vector<float> PoseEstimator::extractDistanceFromDepth(const cv::Mat& depth_image, const std::vector<std::vector<BoundingBox>>& bboxes){
   std::vector<float> distance_vector;
+  std::vector<float> distances;
+  std::vector<float> point(3,0);
+  std::vector<float> pixel(2,0);
+  float z, d;
+  int rows, cols;
+
+#ifdef PROFILE
+  ROS_INFO("Timing distance measurements:");
+  std::chrono::time_point<std::chrono::system_clock> start_distance;
+  std::chrono::time_point<std::chrono::system_clock> end_distance;
+  std::chrono::time_point<std::chrono::system_clock> start_measure;
+  std::chrono::time_point<std::chrono::system_clock> end_measure;
+#endif
+
   if (depth_image.empty()) {
     for (unsigned int i=0; i < bboxes[0].size(); i++) {
       if (!bboxes[0][i].valid_) {
@@ -28,42 +114,80 @@ std::vector<float> PoseEstimator::extractDistanceFromDepth(const cv::Mat& depth_
   }
   size_t reject, keep;
   for (unsigned int i=0; i < bboxes[0].size(); i++) {
+#ifdef PROFILE
+    start_distance = std::chrono::system_clock::now();
+#endif
     if (!bboxes[0][i].valid_) {
       distance_vector.push_back(-1);
+#ifdef PROFILE
+      end_distance = std::chrono::system_clock::now();
+      ROS_INFO(" - Obj %d distance done in %d us", i, std::chrono::duration_cast<std::chrono::microseconds>(end_distance - start_distance).count());
+#endif
       continue;
     }
-    std::vector<float> distances;
+#ifdef PROFILE
+    start_measure = std::chrono::system_clock::now();
+#endif
+    rows = bboxes[0][i].y_max_ - bboxes[0][i].y_min_;
+    cols = bboxes[0][i].x_max_ - bboxes[0][i].x_min_;
+    distances.clear();
+    //distances.resize(rows*cols, 0);
     for (unsigned int row = bboxes[0][i].y_min_; row<bboxes[0][i].y_max_; row++) {
       for (unsigned int col = bboxes[0][i].x_min_; col<bboxes[0][i].x_max_; col++) {
-        distances.push_back(depth_image.at<float>(row,col));
+        z = depth_image.at<float>(row, col);
+        if (z != 0) {
+          pixel[0] = row;
+          pixel[1] = col;
+#ifdef BROWNCONRADY
+          deprojectPixel2PointBrownConrady(z, pixel, point);
+#else
+          deprojectPixel2PointPinHole(z, pixel, point);
+#endif
+          d = sqrt(point[0]*point[0] + point[1]*point[1] + point[2]*point[2]);
+          //distances[(row - bboxes[0][i].y_min_)*cols + (col - bboxes[0][i].x_min_)] = d;
+          distances.push_back(d);
+        }
       }
     }
+#ifdef PROFILE
+    end_measure = std::chrono::system_clock::now();
+#endif
     reject = distances.size() * rejection_threshold_;
     keep = distances.size() * keep_threshold_;
     std::sort(distances.begin(), distances.end(), std::less<float>());
     distance_vector.push_back(std::accumulate(distances.begin() + reject, distances.begin() + reject+keep,0.0) / keep);
+#ifdef PROFILE
+    end_distance = std::chrono::system_clock::now();
+    ROS_INFO(" - Obj %d distance time %d us", i, std::chrono::duration_cast<std::chrono::microseconds>(end_distance - start_distance).count());
+    ROS_INFO("   + measure time %d us", std::chrono::duration_cast<std::chrono::microseconds>(end_measure - start_measure).count());
+    ROS_INFO("   + sort time %d us", std::chrono::duration_cast<std::chrono::microseconds>(end_distance - end_measure).count());
+#endif
   }
   return distance_vector;
 }
 
+
 std::vector<std::vector<float>> PoseEstimator::estimatePosition(const std::vector<float>& distances, const std::vector<std::vector<BoundingBox>>& bboxes) {
   std::vector<std::vector<float>> points;
+  std::vector<float> point(3,0);
+  std::vector<float> pixel(2,0);
   for (unsigned int i=0; i < bboxes[0].size(); i++) {
     float theta, phi;
     if (!bboxes[0][i].valid_) {
-      P_[0] = 0;
-      P_[1] = 0;
-      P_[2] = 0;
-      points.push_back(P_);
+      point[0] = 0;
+      point[1] = 0;
+      point[2] = 0;
+      points.push_back(point);
       continue;
     }
-    phi = horizontal_fov_ * ((bboxes[0][i].x_ / image_width_) - 0.5);
-    theta = vertical_fov_ * (0.5 - (bboxes[0][i].y_ / image_height_));
-    ROS_INFO("%.3f, %.3f, %3.f, %3.f", phi, theta, bboxes[0][i].x_, bboxes[0][i].y_);
-    P_[0] = distances[i] * cos(phi) * sin(theta);
-    P_[1] = distances[i] * sin(phi) * sin(theta);
-    P_[2] = distances[i] * cos(theta);
-    points.push_back(P_);
+    pixel[0] = bboxes[0][i].x_;
+    pixel[1] = bboxes[0][i].y_;
+#ifdef BROWNCONRADY
+    distancePixel2PointBrownConrady(distances[i], pixel, point);
+#else
+    distancePixel2PointPinHole(distances[i], pixel, point);
+#endif
+    points.push_back(point);
   }
   return points;
 }
