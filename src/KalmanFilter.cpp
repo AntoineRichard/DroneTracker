0aef091712dac5d11ccad6d38836840802867f2d
AntoineRichard <antoine.richard@gatech.edu>
2022-09-21 08:46:35 +0200

    added some color to terminal output

--- src/KalmanFilter.cpp
+++ src/KalmanFilter.cpp
@@ -31,18 +31,18 @@ void BaseKalmanFilter::getUncertainty(std::vector<float>& uncertainty) {
 
 void BaseKalmanFilter::predict(const float& dt) {
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Updating.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Updating.\n", __func__, __LINE__); 
 #endif
   updateF(dt);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d F matrix:\n", __func__, __LINE__);
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d F matrix:\n", __func__, __LINE__);
   printF();
-  printf("[DEBUG] KalmanFilter::%s::l%d X (state) before update:\n", __func__, __LINE__);
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d X (state) before update:\n", __func__, __LINE__);
   printX();
 #endif
   X_  = F_ * X_;
 # ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d X (state) after update:\n", __func__, __LINE__);
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d X (state) after update:\n", __func__, __LINE__);
   printX();
 #endif
   P_ = F_ * P_ * F_.transpose() + Q_;
@@ -56,42 +56,42 @@ void BaseKalmanFilter::predict() {
 void BaseKalmanFilter::correct(const std::vector<float>& Z){
   Eigen::MatrixXf Y, S, K, I_KH;
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Measuring.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Measuring.\n", __func__, __LINE__); 
 #endif
   getMeasurement(Z);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing Y.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing Y.\n", __func__, __LINE__); 
 #endif
   Y = Z_ - H_ * X_;
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing S.\n", __func__, __LINE__); 
-  printf("[DEBUG] KalmanFilter::%s::l%d R_ : %ld, %ld\n", __func__, __LINE__,R_.rows(), R_.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d H_ : %ld, %ld\n", __func__, __LINE__,H_.rows(), H_.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d P_ : %ld, %ld\n", __func__, __LINE__,P_.rows(), P_.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing S.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d R_ : %ld, %ld\n", __func__, __LINE__,R_.rows(), R_.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d H_ : %ld, %ld\n", __func__, __LINE__,H_.rows(), H_.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d P_ : %ld, %ld\n", __func__, __LINE__,P_.rows(), P_.cols());
 #endif
   S = R_ + (H_ * (P_ * H_.transpose()));
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d S : %ld, %ld\n", __func__, __LINE__,S.rows(), S.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing K.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d S : %ld, %ld\n", __func__, __LINE__,S.rows(), S.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing K.\n", __func__, __LINE__); 
 #endif
   K = P_ * H_.transpose() * S.inverse();
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d K : %ld, %ld\n", __func__, __LINE__,K.rows(), K.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing X.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d K : %ld, %ld\n", __func__, __LINE__,K.rows(), K.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing X.\n", __func__, __LINE__); 
 #endif
   X_ = X_ + K * Y;
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d X_ : %ld, %ld\n", __func__, __LINE__,X_.rows(), X_.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing I_KH.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d X_ : %ld, %ld\n", __func__, __LINE__,X_.rows(), X_.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing I_KH.\n", __func__, __LINE__); 
 #endif
   I_KH = I_ - K*H_;
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d IKH_ : %ld, %ld\n", __func__, __LINE__,I_KH.rows(), I_KH.cols());
-  printf("[DEBUG] KalmanFilter::%s::l%d Computing P.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d IKH_ : %ld, %ld\n", __func__, __LINE__,I_KH.rows(), I_KH.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Computing P.\n", __func__, __LINE__); 
 #endif
   P_ = I_KH * P_ * I_KH.transpose() + K * R_ * K.transpose();
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d P_ : %ld, %ld\n", __func__, __LINE__,P_.rows(), P_.cols());
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d P_ : %ld, %ld\n", __func__, __LINE__,P_.rows(), P_.cols());
 #endif
 }
 
@@ -117,32 +117,32 @@ KalmanFilter2D::KalmanFilter2D(const float& dt, const bool& use_dim, const bool&
 }
 
 void KalmanFilter2D::printF() {
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(0,0), F_(0,1), F_(0,2), F_(0,3), F_(0,4), F_(0,5));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(1,0), F_(1,1), F_(1,2), F_(1,3), F_(1,4), F_(1,5));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(2,0), F_(2,1), F_(2,2), F_(2,3), F_(2,4), F_(2,5));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(3,0), F_(3,1), F_(3,2), F_(3,3), F_(3,4), F_(3,5));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(4,0), F_(4,1), F_(4,2), F_(4,3), F_(4,4), F_(4,5));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(5,0), F_(5,1), F_(5,2), F_(5,3), F_(5,4), F_(5,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(0,0), F_(0,1), F_(0,2), F_(0,3), F_(0,4), F_(0,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(1,0), F_(1,1), F_(1,2), F_(1,3), F_(1,4), F_(1,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(2,0), F_(2,1), F_(2,2), F_(2,3), F_(2,4), F_(2,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(3,0), F_(3,1), F_(3,2), F_(3,3), F_(3,4), F_(3,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(4,0), F_(4,1), F_(4,2), F_(4,3), F_(4,4), F_(4,5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(5,0), F_(5,1), F_(5,2), F_(5,3), F_(5,4), F_(5,5));
 }
 
 void KalmanFilter2D::printX() {
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, X_(0), X_(1), X_(2), X_(3), X_(4), X_(5));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, X_(0), X_(1), X_(2), X_(3), X_(4), X_(5));
 }
 
 void KalmanFilter2D::initialize(const std::vector<float>& Q, const std::vector<float>& R) {
   // State is [u, v, vu ,vv, h, w]
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Creating new 2D Kalman filter.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Creating new 2D Kalman filter.\n", __func__, __LINE__); 
 #endif
   X_ = Eigen::VectorXf(6);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting state to 0.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting state to 0.\n", __func__, __LINE__); 
 #endif
   X_ << 0, 0, 0, 0, 0, 0;
 
   // Process noise
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting Q.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting Q.\n", __func__, __LINE__); 
 #endif
   Q_ = Eigen::MatrixXf::Zero(6,6);
   Q_(0,0) = Q[0];
@@ -154,31 +154,31 @@ void KalmanFilter2D::initialize(const std::vector<float>& Q, const std::vector<f
 
   // Dynamics
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting F.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting F.\n", __func__, __LINE__); 
 #endif
   F_ = Eigen::MatrixXf::Identity(6,6);
   updateF(dt_);
 
   // Covariance
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting P.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting P.\n", __func__, __LINE__); 
 #endif
   P_ = Eigen::MatrixXf::Zero(6,6);
   P_ = Q_*Q_;
 
   // Helper matrices
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting I.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting I.\n", __func__, __LINE__); 
 #endif
   I_ = Eigen::MatrixXf::Identity(6,6);
 
   // Observation noise
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting R.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting R.\n", __func__, __LINE__); 
 #endif
   buildR(R);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting H.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting H.\n", __func__, __LINE__); 
 #endif
   buildH();
 }
@@ -254,8 +254,8 @@ void KalmanFilter2D::buildR(const std::vector<float>& R){
 
 void KalmanFilter2D::resetFilter(const std::vector<float>& initial_state) {
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Reinitializing state and covariance.\n", __func__, __LINE__); 
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting state.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Reinitializing state and covariance.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting state.\n", __func__, __LINE__); 
 #endif
   X_ << initial_state[0],
         initial_state[1],
@@ -265,7 +265,7 @@ void KalmanFilter2D::resetFilter(const std::vector<float>& initial_state) {
         initial_state[5];
 
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting P_.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting P_.\n", __func__, __LINE__); 
 #endif
   P_ = Q_*Q_;
 }
@@ -311,34 +311,34 @@ KalmanFilter3D::KalmanFilter3D(const float& dt, const bool& use_dim, const bool&
 }
 
 void KalmanFilter3D::printF() {
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(0,0), F_(0,1), F_(0,2), F_(0,3), F_(0,4), F_(0,5), F_(0,6), F_(0,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(1,0), F_(1,1), F_(1,2), F_(1,3), F_(1,4), F_(1,5), F_(1,6), F_(1,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(2,0), F_(2,1), F_(2,2), F_(2,3), F_(2,4), F_(2,5), F_(2,6), F_(2,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(3,0), F_(3,1), F_(3,2), F_(3,3), F_(3,4), F_(3,5), F_(3,6), F_(3,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(4,0), F_(4,1), F_(4,2), F_(4,3), F_(4,4), F_(4,5), F_(4,6), F_(4,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(5,0), F_(5,1), F_(5,2), F_(5,3), F_(5,4), F_(5,5), F_(5,6), F_(5,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(6,0), F_(6,1), F_(6,2), F_(6,3), F_(6,4), F_(6,5), F_(6,6), F_(6,7));
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(7,0), F_(7,1), F_(7,2), F_(7,3), F_(7,4), F_(7,5), F_(7,6), F_(7,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(0,0), F_(0,1), F_(0,2), F_(0,3), F_(0,4), F_(0,5), F_(0,6), F_(0,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(1,0), F_(1,1), F_(1,2), F_(1,3), F_(1,4), F_(1,5), F_(1,6), F_(1,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(2,0), F_(2,1), F_(2,2), F_(2,3), F_(2,4), F_(2,5), F_(2,6), F_(2,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(3,0), F_(3,1), F_(3,2), F_(3,3), F_(3,4), F_(3,5), F_(3,6), F_(3,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(4,0), F_(4,1), F_(4,2), F_(4,3), F_(4,4), F_(4,5), F_(4,6), F_(4,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(5,0), F_(5,1), F_(5,2), F_(5,3), F_(5,4), F_(5,5), F_(5,6), F_(5,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(6,0), F_(6,1), F_(6,2), F_(6,3), F_(6,4), F_(6,5), F_(6,6), F_(6,7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, F_(7,0), F_(7,1), F_(7,2), F_(7,3), F_(7,4), F_(7,5), F_(7,6), F_(7,7));
 }
 
 void KalmanFilter3D::printX() {
-  printf("[DEBUG] KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, X_(0), X_(1), X_(2), X_(3), X_(4), X_(5), X_(6), X_(7));
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f\n", __func__, __LINE__, X_(0), X_(1), X_(2), X_(3), X_(4), X_(5), X_(6), X_(7));
 }
 
 void KalmanFilter3D::initialize(const std::vector<float>& Q, const std::vector<float>& R) {
   // State is [x, y, z, vx ,vy, vz, h, w]
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Creating new 3D Kalman filter.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Creating new 3D Kalman filter.\n", __func__, __LINE__); 
 #endif
   X_ = Eigen::VectorXf(8);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting state to 0.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting state to 0.\n", __func__, __LINE__); 
 #endif
   X_ << 0, 0, 0, 0, 0, 0, 0, 0;
 
   // Process noise
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting Q.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting Q.\n", __func__, __LINE__); 
 #endif
   Q_ = Eigen::MatrixXf::Zero(8,8);
   Q_(0,0) = Q[0];
@@ -352,31 +352,31 @@ void KalmanFilter3D::initialize(const std::vector<float>& Q, const std::vector<f
 
   // Dynamics
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting F.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting F.\n", __func__, __LINE__); 
 #endif
   F_ = Eigen::MatrixXf::Identity(8,8);
   updateF(dt_);
 
   // Covariance
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting P.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting P.\n", __func__, __LINE__); 
 #endif
   P_ = Eigen::MatrixXf::Zero(8,8);
   P_ = Q_*Q_;
 
   // Helper matrices
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting I.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting I.\n", __func__, __LINE__); 
 #endif
   I_ = Eigen::MatrixXf::Identity(8,8);
 
   // Observation noise
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting R.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting R.\n", __func__, __LINE__); 
 #endif
   buildR(R);
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting H.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting H.\n", __func__, __LINE__); 
 #endif
   buildH();
 }
@@ -457,8 +457,8 @@ void KalmanFilter3D::buildR(const std::vector<float>& R){
 
 void KalmanFilter3D::resetFilter(const std::vector<float>& initial_state) {
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Reinitializing state and covariance.\n", __func__, __LINE__); 
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting state.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Reinitializing state and covariance.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting state.\n", __func__, __LINE__); 
 #endif
   X_ << initial_state[0],
         initial_state[1],
@@ -470,7 +470,7 @@ void KalmanFilter3D::resetFilter(const std::vector<float>& initial_state) {
         initial_state[7];
 
 #ifdef DEBUG_KALMAN
-  printf("[DEBUG] KalmanFilter::%s::l%d Setting P_.\n", __func__, __LINE__); 
+  printf("\e[1;33m[DEBUG  ]\e[0m KalmanFilter::%s::l%d Setting P_.\n", __func__, __LINE__); 
 #endif
   P_ = Q_*Q_;
 }
